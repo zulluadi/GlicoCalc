feac948b1ebd7e21adb1e9f5ea193b5add92af4b
Adrian Sulumberchean <zulluadi@gmail.com>
2026-04-19 13:49:33 +0300

    build: bump version to 0.3 to trigger clean app update

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -58,8 +58,8 @@ android {
         applicationId = "com.glicocalc"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
     }
     buildTypes {
         getByName("release") {
